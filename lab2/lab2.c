a6d13b61e7a42cdea6e0bcd20376cd0bd6d9e02f
EnzeChen0314 <ec3576@columbia.edu>
2022-03-02 18:03:24 -0500

    Update lab2.c

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -168,8 +168,8 @@ int main()
       }
       if ((int)send0 == 177) { write(sockfd, sendram, pos1); pos1 = 0; ramclear();} 
       
-      if ((int)send0 == 179) sendfull = 0;
-      if ((int)send0 == 180) sendfull = 0;
+      if ((int)send0 == 179) {sendfull = 0; del();}
+      if ((int)send0 == 180) {sendfull = 0; golast();}
 			      
       if (packet.keycode[0] == 0x29) { /* ESC pressed? */
 	      break;

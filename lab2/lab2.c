3c2c531ca90dfb98d3e703fdb66fea15955d9075
EnzeChen0314 <ec3576@columbia.edu>
2022-03-02 14:31:03 -0500

    Update lab2.c

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -158,7 +158,7 @@ int main()
 	  }
 	}
       }
-      if ((int)send0 == 177) { write(sockfd, sendram, 2*MAX_COL); ramclear();} 
+      if ((int)send0 == 177) { write(sockfd, sendram, pos); ramclear();} 
 			      
       if (packet.keycode[0] == 0x29) { /* ESC pressed? */
 	      break;
